39a7f42fb6c2632180cf86276f07835444dd3dc1
Junior Martins <amjr.box@gmail.com>
2026-01-11 19:57:24 -0300

    feat(server): adiciona configuração inicial do servidor Fastify

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import multipart from "@fastify/multipart";
+import Fastify from "fastify";
+import corsPlugin from "./middleware/cors.js";
+import loggerPlugin from "./middleware/logger.js";
+import authRoutes from "./routes/auth.routes.js";
+import chartsRoutes from "./routes/charts.routes.js";
+import panelsRoutes from "./routes/panels.routes.js";
+import queryRoutes from "./routes/query.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+export const buildServer = async () => {
+  const app = Fastify({ logger: true });
+
+  await app.register(corsPlugin);
+  await app.register(multipart);
+  await app.register(loggerPlugin);
+
+  await app.register(authRoutes, { prefix: "/authentication" });
+  await app.register(userRoutes, { prefix: "/me" });
+  await app.register(panelsRoutes, { prefix: "/panels" });
+  await app.register(chartsRoutes, { prefix: "/charts" });
+  await app.register(queryRoutes, { prefix: "/query" });
+
+  return app;
+};
