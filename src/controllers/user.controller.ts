30e826816b3adf5610289656095a5e6e4302d238
Junior Martins <amjr.box@gmail.com>
2026-01-11 19:50:17 -0300

    feat(user): adiciona endpoint para obter dados do usuário autenticado

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,19 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { store } from "../store.js";
+import type { UserResponse } from "../types/index.js";
+
+const toUserResponse = (user: (typeof store.users)[number]): UserResponse => {
+  const { password, ...rest } = user;
+  return rest;
+};
+
+export const getMe = async (request: FastifyRequest, reply: FastifyReply) => {
+  const user = request.user ?? store.users[0];
+
+  if (!user) {
+    reply.status(404).send({ error: "User not found" });
+    return;
+  }
+
+  reply.send(toUserResponse(user));
+};
