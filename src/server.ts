c50e53e9f95d0106f8629e5002ee6b46707f0d38
Junior Martins <amjr.box@gmail.com>
2026-01-12 08:29:03 -0300

    feat(server): adiciona rotas de autorização

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import corsPlugin from "./middleware/cors.js";
 import loggerPlugin from "./middleware/logger.js";
 import proxyPlugin from "./middleware/proxy.js";
 import authRoutes from "./routes/auth.routes.js";
+import authorizationRoutes from "./routes/authorization.routes.js";
 import chartsRoutes from "./routes/charts.routes.js";
 import panelsRoutes from "./routes/panels.routes.js";
 import queryRoutes from "./routes/query.routes.js";
@@ -18,6 +19,7 @@ export const buildServer = async () => {
   await app.register(proxyPlugin);
 
   await app.register(authRoutes, { prefix: "/authentication" });
+  await app.register(authorizationRoutes, { prefix: "/authorization" });
   await app.register(userRoutes, { prefix: "/me" });
   await app.register(panelsRoutes, { prefix: "/panels" });
   await app.register(chartsRoutes, { prefix: "/charts" });
